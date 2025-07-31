8523035fa6de35781555937a3e9857919fda4456
Artem Chuvaiev <137652420+tmprokill@users.noreply.github.com>
2025-07-31 18:17:13 +0200

    shareboard.back

---
+++ Back/ShareBoard/ShareBoard.API/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShareBoard.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+}
